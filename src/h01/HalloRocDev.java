7ccaa83d9379bae2c9470a1ce78863c7148c2ccb
abigailao <abigail.ao@outlook.com>
2017-09-18 18:57:32 +0200

    Hoofdstuk 1 Opdracht 4

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -1,11 +1,11 @@
 package h01;
 
 /**
- * Edited multiple by Abigail on 18-09-17.
+ * Edited multiple times by Abigail on 18-09-17.
  */
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Corrected");
+        System.out.println("Another edit.");
     }
 }
