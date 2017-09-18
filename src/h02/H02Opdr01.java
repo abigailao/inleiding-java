4335090e3827c2de5bf36909503b3330fe93d7fa
abigailao <abigail.ao@outlook.com>
2017-09-18 18:32:48 +0200

    Hoofdstuk 2 Opdracht 1

---
+++ src/h02/H02Opdr01.java
@@ -0,0 +1,21 @@
+package h02;
+
+/*
+* Maak een applet in Java waarin je naam in het geel wordt afgedrukt op een blauw achtergrond.
+ */
+
+import java.awt.*;
+import java.applet.*;
+
+public class H02Opdr01 extends Applet {
+
+    public void init() {
+        setBackground(Color.blue);
+    }
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.drawString("Abigail", 190,160);
+    }
+
+
+}
