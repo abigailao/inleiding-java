6f611de4c640468d8bef00f2974adecf54209f44
abigailao <abigail.ao@outlook.com>
2017-09-18 18:30:50 +0200

    Hoofdstuk 2 Opdracht 1

---
+++ src/h02/H02Opdr02.java
@@ -0,0 +1,19 @@
+package h02;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H02Opdr02 extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+            g.setColor(Color.blue);
+            g.drawString("Abigail", 190, 160);
+        g.setColor(Color.red);
+            g.drawString("Akhile Oijiangbe", 190, 175);
+
+        }
+    }
