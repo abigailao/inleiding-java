6e4aedbe54380842e3d0ecc1a4b1589f240e4d92
abigailao <abigail.ao@outlook.com>
2017-09-18 18:55:11 +0200

    Hoofdstuk 1 Opdracht 3

---
+++ src/h01/MijnEersteJavaKlasse.java
@@ -0,0 +1,11 @@
+package h01;
+
+/**
+ *Redone multiple times by Abigail on 18-09-2017
+ */
+
+public class MijnEersteJavaKlasse {
+    public static void main(String[] args) {
+        System.out.println("Dit is mijn eerste java klasse!");
+    }
+}
