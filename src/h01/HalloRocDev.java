062a8e5562bd0ddfaafd464e9259f6d56f507b10
abigailao <abigail.ao@outlook.com>
2017-09-18 11:12:10 +0200

    Hoofdstuk 1 Opdracht 4

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -1,11 +1,11 @@
 package h01;
 
 /**
- * Edited by Abigail on 18-09-17.
+ * Edited twice by Abigail on 18-09-17.
  */
 public class HalloRocDev {
 
     public static void main(String[] args) {
-System.out.println("Y tho.");
+        System.out.println("Second Attempt.");
     }
 }
