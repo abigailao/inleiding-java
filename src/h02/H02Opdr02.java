503bce9803d4c57e333e444642f72700e0189530
abigailao <abigail.ao@outlook.com>
2017-09-18 19:10:53 +0200

    Hoofdstuk 2 Opdracht 2

--- src/h02/H02Opdr02.java
+++ src/h02/H02Opdr02.java
@@ -1,5 +1,8 @@
 package h02;
 
+// Maak een applet in Java waarin je roepnaam en je achternaam onder elkaar worden afgedrukt.
+//De achtergrond kleur is wit, je roepnaam wordt in blauw en je achternaam in rood afgedrukt op het scherm.
+
 import java.awt.*;
 import java.applet.*;
 
