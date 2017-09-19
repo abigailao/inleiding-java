434f3aece99a3a2afbdc7476b2276825fc5899f5
abigailao <abigail.ao@outlook.com>
2017-09-19 09:59:45 +0200

    Hoofdstuk 4 Opdracht 1

---
+++ src/h04/H04Opdr01.java
@@ -0,0 +1,16 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class H04Opdr01 extends Applet {
+
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.drawPolygon(new int[] {180, 200, 220},new int[] {160, 40, 160}, 3);
+    }
+
+}
