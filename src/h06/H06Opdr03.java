4265860e936f13c6b481106298d53021d78cf925
abigailao <abigail.ao@outlook.com>
2017-10-01 14:47:53 +0200

    Hoofdstuk 6 Opdracht 3

---
+++ src/h06/H06Opdr03.java
@@ -0,0 +1,28 @@
+package h06;
+
+// Opdracht 6.3: Schrijf een applet waarin je twee ints met positieve waarden bij elkaar optelt en er een negatieve int uit komt.
+
+import java.awt.*;
+import java.applet.*;
+
+public class H06Opdr03 extends Applet {
+
+    int a;
+    int b;
+    int c;
+    int negative;
+
+
+    public void init() {
+        a = 1;
+        b = 2;
+        c = a + b;
+        negative = --c;
+
+    }
+    public void paint(Graphics g){
+        g.drawString("answer -" + --c, 50, 50 );
+    }
+
+
+}
