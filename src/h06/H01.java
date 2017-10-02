6e2383677d7324059dee05e28a7178e7a7b05709
abigailao <abigail.ao@outlook.com>
2017-10-02 13:29:13 +0200

    Hoofdstuk 6 Opdracht 1

--- src/h06/H01.java
+++ src/h06/H01.java
@@ -11,22 +11,21 @@ Schrijf een applet waarin je een berekening maakt om het geld zo eerlijk mogelij
 In het venster van de applet komen de vier namen onder elkaar te staan met daarachter het bedrag dat een ieder krijgt.
  */
 
-    private int x;
+private double uitkomst;
 
     public void init() {
-       x = 113 / 4;
-       String Jan = "Jan ";
-       String Ali = "Ali ";
-       String Jeannette = "Jeannette ";
-       String Abigail = "Abigail ";
+         double getal = 113;
+         double getal2 = 4;
+         uitkomst = getal / getal2;
+
     }
 
     //namen onder elkaar met verdeelde bedragen erachter.
     public void paint(Graphics g) {
-        g.drawString("Jan " +  x, 50,50);
-        g.drawString("Ali " + x, 50,70);
-        g.drawString("Jeannette " + x, 50, 90);
-        g.drawString("Abigail " + x, 50, 110);
+        g.drawString("Jan " + uitkomst, 50,50);
+        g.drawString("Ali " + uitkomst, 50,70);
+        g.drawString("Jeannette " + uitkomst, 50, 90);
+        g.drawString("Abigail " + uitkomst, 50, 110);
     }
 
 }
