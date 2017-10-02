eae63a17b1f2384ecac63ae02a24fba4ce34b1bb
abigailao <abigail.ao@outlook.com>
2017-10-02 12:13:35 +0200

    Hoofdstuk 6 Opdracht 1

--- src/h06/H01.java
+++ src/h06/H01.java
@@ -6,23 +6,19 @@ import java.awt.*;
 
 public class H01 extends Applet{
 
-/*Opdracht 6.1: Met z'n vieren (Jan, Ali, Jeannette en jij) hebben jullie een karwei verricht en daarmee 113 euro verdiend.
+/**Opdracht 6.1: Met z'n vieren (Jan, Ali, Jeannette en jij) hebben jullie een karwei verricht en daarmee 113 euro verdiend.
 Schrijf een applet waarin je een berekening maakt om het geld zo eerlijk mogelijk te verdelen.
 In het venster van de applet komen de vier namen onder elkaar te staan met daarachter het bedrag dat een ieder krijgt.
  */
 
-    int x;
-    String Jan;
-    String Ali;
-    String Jeannette;
-    String Abigail;
+    private int x;
 
     public void init() {
        x = 113 / 4;
-       Jan = "Jan ";
-       Ali = "Ali ";
-       Jeannette = "Jeannette ";
-       Abigail = "Abigail ";
+       String Jan = "Jan ";
+       String Ali = "Ali ";
+       String Jeannette = "Jeannette ";
+       String Abigail = "Abigail ";
     }
 
     //namen onder elkaar met verdeelde bedragen erachter.
