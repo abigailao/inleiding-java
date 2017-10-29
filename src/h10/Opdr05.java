248e359ccaa752bb463af12a7b4712333081c2a8
abigailao <abigail.ao@outlook.com>
2017-10-29 21:42:06 +0100

    Hoofdstuk 10 Opdracht 5

--- src/h10/Opdr05.java
+++ src/h10/Opdr05.java
@@ -1,60 +1,40 @@
 package h10;
 
 import java.applet.Applet;
-
 import java.awt.*;
-
-import java.awt.event.ActionEvent;
-
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 
 
 public class Opdr05 extends Applet{
-
     double gem1, gem2, newgetal, getal;
-
-    Button knop1;
-
-    Button knop2;
-
+    Button knop1, knop2;
     TextField vak1;
 
 
 
     public void init() {
-
         setSize(400,200);
 
         vak1 = new TextField("",10);
-
+        knop1 = new Button("Voeg een cijfer toe.");
         knop2 = new Button("reset");
 
         gem1 = 0;
-
         newgetal = 0;
-
         getal = 0;
 
-        Listen1 nc = new Listen1(); //Add a new number
-
-        Listen2 re = new Listen2(); //Reset
-
-        knop1 = new Button("Voeg een cijfer toe.");
-
-        knop1.addActionListener(nc);
-
-        knop2.addActionListener(re);
+        knop1.addActionListener(new Listen1());
+        knop2.addActionListener(new Listen2());
 
         add(vak1);
-
         add(knop1);
-
         add(knop2);
     }
 
 
     public void paint(Graphics g) {
+        g.drawString("uw gemiddelde is: " + (String.format ("%.2f", Double.valueOf(gem2))), 100, 150);
 
         if(gem1<0.01)
         {
@@ -74,47 +54,30 @@ public class Opdr05 extends Applet{
         else if(gem2>5.45 && gem2<10.1)
         {
             g.drawString("U bent geslaagd.",100,100); }
-
-
-        g.drawString("uw gemiddelde is: " + (String.format ("%.2f", Double.valueOf(gem2))), 100, 150);
     }
-
 
 
     class Listen1 implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-
             newgetal = Double.parseDouble(vak1.getText());
-
             gem1++;
-
             getal = getal + newgetal;
-
             gem2 = getal/gem1;
 
             repaint();
         }
-
     }
-
-
 
     class Listen2 implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-
             newgetal = 0;
-
             gem1 = 0;
-
             gem2 = 0;
-
             getal = 0;
-
             repaint();
         }
-
     }
 
 }
