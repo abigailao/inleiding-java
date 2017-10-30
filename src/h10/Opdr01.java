c5bca8bb42a726868849859aa686c7ce0275d1fe
abigailao <abigail.ao@outlook.com>
2017-10-30 12:02:29 +0100

    Hoofdstuk 10 Opdracht 1

--- src/h10/Opdr01.java
+++ src/h10/Opdr01.java
@@ -5,33 +5,33 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Opdr01 extends Applet {
-    TextField tekstvak;
-    double getal1, getal2;
-    String s;
+    TextField Tekstvak;
+    double Getal1, Getal2;
+    String S;
 
 
     public void init() {
         setSize(400,100);
 
-        tekstvak = new TextField("",10);
-        add(tekstvak);
-        tekstvak.addActionListener(new aListener());
+        Tekstvak = new TextField("",10);
+        add(Tekstvak);
+        Tekstvak.addActionListener(new aListener());
 
-        s = "";
+        S = "";
     }
 
     public void paint(Graphics g) {
         g.drawString("Vul getal in:", 90,20);
-        g.drawString("Hoogste getal tot nu toe: " + getal1, 90, 40);
+        g.drawString("Hoogste getal tot nu toe: " + Getal1, 90, 40);
 
     }
 
     class aListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            getal2 = Double.parseDouble(tekstvak.getText());
-            if ((getal2 > getal1)) {
-                getal1 = getal2;
-                tekstvak.setText(null);
+            Getal2 = Double.parseDouble(Tekstvak.getText());
+            if ((Getal2 > Getal1)) {
+                Getal1 = Getal2;
+                Tekstvak.setText(null);
                 repaint();
             }
         }
