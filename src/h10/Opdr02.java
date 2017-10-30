ed4083f3650b5fb14a38cab8c4413ea8530c1336
abigailao <abigail.ao@outlook.com>
2017-10-30 12:08:00 +0100

    Hoofdstuk 10 Opdracht 2

--- src/h10/Opdr02.java
+++ src/h10/Opdr02.java
@@ -7,41 +7,41 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Opdr02 extends Applet {
-    TextField tekstvak;
-    double getal1, getal2, getal3;
+    TextField Tekstvak;
+    double Getal1, Getal2, Getal3;
 
 
     public void init() {
         setSize(400,100);
 
-        tekstvak = new TextField("",10);
-        add(tekstvak);
-        tekstvak.addActionListener(new aListener());
+        Tekstvak = new TextField("",10);
+        add(Tekstvak);
+        Tekstvak.addActionListener(new aListener());
 
     }
 
     public void paint(Graphics g) {
         g.drawString("Vul getal in:", 90,20);
-        g.drawString("Hoogste getal tot nu toe: " + getal1, 90, 50);
-        g.drawString("Laagste getal tot nu toe: " + getal3, 90,70);
+        g.drawString("Hoogste getal tot nu toe: " + Getal1, 90, 50);
+        g.drawString("Laagste getal tot nu toe: " + Getal3, 90,70);
 
     }
 
     class aListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            getal2 = Double.parseDouble(tekstvak.getText());
-            if (getal3 == 0)
+            Getal2 = Double.parseDouble(Tekstvak.getText());
+            if (Getal3 == 0)
             {
-                getal3 = getal2;
+                Getal3 = Getal2;
             }
-            if ((getal2 > getal1)) {
-                getal1 = getal2;
-                tekstvak.setText(null);
+            if ((Getal2 > Getal1)) {
+                Getal1 = Getal2;
+                Tekstvak.setText(null);
                 repaint();
             }
-            if ((getal2 < getal1) && (getal2 < getal3)) {
-                getal3 = getal2;
-                tekstvak.setText(null);
+            if ((Getal2 < Getal1) && (Getal2 < Getal3)) {
+                Getal3 = Getal2;
+                Tekstvak.setText(null);
                 repaint();
             }
         }
